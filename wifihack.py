16a0f4cbfc2b4dfb72636716fc415f0f64987a7c
younglord02 <111462595+younglord02@users.noreply.github.com>
2022-09-01 13:56:39 +0200

    Add files via upload

---
+++ wifihack.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+from subprocess import check_call
+print("\nInstalling Needed Tools")
+print("\n")
+cmd0 = os.system("apt-get install aircrack-ng crunch xterm wordlists reaver pixiewps bully xterm wifite")
+cmd = os.system("sleep3 && clear")
+def intro():
+  cmd = os.system("clear")
+  print("""\033[1;32m
+  
